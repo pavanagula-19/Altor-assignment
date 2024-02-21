8ce4fbc0f792431692f7c0135d9eeca5b8ded64f
Pavanagula-19 <Pavanagulla19@gmail.com>
2024-02-21 23:25:48 +0530

    database fetched to deploy

--- server/server.js
+++ server/server.js
@@ -1,28 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const DataModel = require('./models/Schema');
 const dotenv = require('dotenv');
-
+const cors = require('cors')
+const bodyParser = require('body-parser'); 
 dotenv.config();
+const DataModel = require('./models/Schema')
+
 const app = express();
-app.use(cors());
-
-const database = mongoose.connect(process.env.DATABASE_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
-
+const database = require('./database');
+database.db;
 app.get('/data', async (req, res) => {
   try {
     const data = await DataModel.find();
 
     if (data && data.length > 0) {
       console.log('Data fetched from MongoDB');
-      res.status(200).json({ success: true, data });
+      res.status(200).json({ data });
     } else {
       console.error('No data found in the database');
       res.status(404).json({ success: false, message: 'No data found in the database' });
@@ -32,7 +24,6 @@ app.get('/data', async (req, res) => {
     res.status(500).json({ success: false, message: 'Error fetching data from the database' });
   }
 });
-
 app.listen(process.env.PORT, () => {
   console.log(`Server Running At ${process.env.PORT}`);
 });
